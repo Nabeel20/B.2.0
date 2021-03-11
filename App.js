a404446d07b67f1444843483fb983414e454396b
Nabeel20 <nabeelandnizam@gmail.com>
2021-03-11 09:02:44 +0200

    Pushing Files

---
+++ App.js
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View } from 'react-native'
+const App = () => {
+  return (
+    <View>
+
+    </View>
+  )
+}
+export default App;
